b18b9dc04ab434029d47e09b2f584f8fc5192293
unknown <thwong2@697-220-Sil.sf.k12.internal>
2015-09-16 10:46:53 -0700

    Made multiple things, need to add colors and biased movements

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -1,30 +1,50 @@
- Bacteria cell;
+ Bacteria [] cell;
+ int setupSize = 400;
  //declare bacteria variables here   
  void setup()   
  {     
- 	cell = new Bacteria(100,100);
- 	size(400,400);
+ 	cell = new Bacteria [20];	
+ 	for(int x= 0;x<cell.length;x++)
+ 	{
+ 		int xPos=(int)(Math.random()*setupSize);
+ 		int yPos= (int)(Math.random()*setupSize);
+ 		int big = ((int)(Math.random()*20))+5;
+ 		cell[x]= new Bacteria(xPos,yPos,big);
+ 	}
+ 	size(setupSize,setupSize);
  }	//initialize bacteria variables here      
  void draw()   
  {    
- 	background(255);
- 	fill(255);	
- 	cell.show();
- 	cell.move();
+ 	background(0,0,0,50);
+ 	for(int a = 0;a < cell.length;a++)
+ 	{
+	 	cell[a].show();
+	 	cell[a].move();
+ 	}
  	//move and show the bacteria   
  }  
+  color randomColor()
+ 	{
+  	return color((int)(Math.random()*255),(int)(Math.random()*255),
+    (int)(Math.random()*255));//random colors
+  	}
  class Bacteria    
  {     
- 	int bacX,bacY;
- 	Bacteria(int x,int y){
+ 	int bacX,bacY,bacColor,bacSize;
+ 	Bacteria(int x,int y,int size)
+ 	{
  		bacX=x;
  		bacY=y;
+ 		bacSize=size;
  	}	
- 	void show(){
- 		ellipse(bacX,bacY,10,10);
+ 	void show()
+ 	{
+ 		fill(0);//empty in center
+ 		ellipse(bacX,bacY,bacSize,bacSize);
  	}
- 	void move(){
- 		cell.bacX=cell.bacX+((int)(Math.random()*3)-1);//-1,0,1,
- 		cell.bacY=cell.bacY+((int)(Math.random()*3)-1);//-1,0,1,
+ 	void move()
+ 	{
+ 		bacX=bacX+((int)(Math.random()*3)-1);//-1,0,1,
+ 		bacY=bacY+((int)(Math.random()*3)-1);//-1,0,1,
  	}
  }    
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -14,34 +14,55 @@ import java.io.IOException;
 
 public class Chemotaxis extends PApplet {
 
- Bacteria cell;
+ Bacteria [] cell;
+ int setupSize = 400;
  //declare bacteria variables here   
  public void setup()   
  {     
- 	cell = new Bacteria(100,100);
- 	size(400,400);
+ 	cell = new Bacteria [20];	
+ 	for(int x= 0;x<cell.length;x++)
+ 	{
+ 		int xPos=(int)(Math.random()*setupSize);
+ 		int yPos= (int)(Math.random()*setupSize);
+ 		int big = ((int)(Math.random()*20))+5;
+ 		cell[x]= new Bacteria(xPos,yPos,big);
+ 	}
+ 	size(setupSize,setupSize);
  }	//initialize bacteria variables here      
  public void draw()   
  {    
- 	background(255);
- 	fill(255);	
- 	cell.show();
- 	cell.move();
+ 	background(0,0,0,50);
+ 	for(int a = 0;a < cell.length;a++)
+ 	{
+	 	cell[a].show();
+	 	cell[a].move();
+ 	}
  	//move and show the bacteria   
  }  
+  public int randomColor()
+ 	{
+  	return color((int)(Math.random()*255),(int)(Math.random()*255),
+    (int)(Math.random()*255));//random colors
+  	}
  class Bacteria    
  {     
- 	int bacX,bacY;
- 	Bacteria(int x,int y){
+ 	int bacX,bacY,bacColor,bacSize;
+ 	Bacteria(int x,int y,int size)
+ 	{
+ 		stroke(randomColor());
  		bacX=x;
  		bacY=y;
+ 		bacSize=size;
  	}	
- 	public void show(){
- 		ellipse(bacX,bacY,10,10);
+ 	public void show()
+ 	{
+ 		fill(0);//empty in center
+ 		ellipse(bacX,bacY,bacSize,bacSize);
  	}
- 	public void move(){
- 		cell.bacX=cell.bacX+((int)(Math.random()*3)-1);//-1,0,1,
- 		cell.bacY=cell.bacY+((int)(Math.random()*3)-1);//-1,0,1,
+ 	public void move()
+ 	{
+ 		bacX=bacX+((int)(Math.random()*3)-1);//-1,0,1,
+ 		bacY=bacY+((int)(Math.random()*3)-1);//-1,0,1,
  	}
  }    
   static public void main(String[] passedArgs) {
